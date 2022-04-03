e269d4115f8e5bb5912a8023afbf6f836cbea52b
Ety-Biswangri <etybgc02@gmail.com>
2022-04-04 00:08:18 +0600

    home page first part is done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import pickles from '../../Assets/Images/pickles.jpg';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
-            <h2>Home Page</h2>
+            <div className='home-page-first-portion'>
+                <div className='home-page-first-portion-left'>
+                    <div style={{ marginLeft: '100px' }}>
+                        <h1>Your Favourite Pickles</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus.
+                        </p>
+                        <button>Explore Now</button>
+                    </div>
+                </div>
+                <div className='home-page-first-portion-right'>
+                    <img src={pickles} alt="pickleImage" />
+                </div>
+            </div>
+            <div className='home-page-second-portion'>
+                <h2>Customer Reviews</h2>
+            </div>
         </div>
     );
 };
