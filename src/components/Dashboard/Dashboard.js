ddaae61450d1722727bc1ba31340b553bbed4cc8
Ety-Biswangri <etybgc02@gmail.com>
2022-04-04 16:49:38 +0600

    dashboard styles are improved

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Bar, BarChart, CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import './Dashboard.css';
 
 const Dashboard = () => {
@@ -21,6 +21,7 @@ const Dashboard = () => {
                         <Line dataKey={'sell'}></Line>
                         <XAxis dataKey={'month'} stroke='rgb(25, 25, 112)'></XAxis>
                         <Tooltip></Tooltip>
+                        <Legend />
                         <YAxis></YAxis>
                     </LineChart>
                 </div>
@@ -28,13 +29,14 @@ const Dashboard = () => {
             <div className='chart-right'>
                 <div>
                     <h2 style={{ textAlign: 'center', marginBottom: '30px', color: 'rgb(25, 25, 112)' }}>Investment Vs Revenue</h2>
-                    <BarChart width={600} height={400} data={data}>
+                    <BarChart width={500} height={400} data={data}>
                         <XAxis dataKey="month" stroke='rgb(25, 25, 112)' />
                         <YAxis />
                         <Tooltip />
+                        <Legend />
                         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                        <Bar dataKey="investment" fill="#1f77b4" barSize={30} />
-                        <Bar dataKey="revenue" fill="#2ca02c" barSize={30} />
+                        <Bar dataKey="investment" stackId="a" fill="#1f77b4" barSize={30} />
+                        <Bar dataKey="revenue" stackId="a" fill="#2ca02c" barSize={30} />
                     </BarChart>
                 </div>
             </div>
